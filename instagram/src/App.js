9198a1e630770a0ac2532e2eeb45e824e27d1416
unknown <lappjeff20@gmail.com>
2019-03-25 15:38:00 -0400

    Added react-strap and set up basic imports for Nav/Search bar

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,10 +3,17 @@ import './App.scss';
 
 import {SearchBar, PostContainer, CommentSection} from './components'
 
+//react-strap imports
+import {NavBar, NavItem, NavLink} from 'reactstrap'
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className='container'>
+          <SearchBar />
+          <PostContainer />
+          <CommentSection />
+        </div>
       </div>
     );
   }
--- instagram/src/index.js
+++ instagram/src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
+//Bootstrap react-strap import
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
